cf5423ae89e2a34f34572fafe67b747f407ecb56
Szymon <01178585@pw.edu.pl>
2025-03-07 19:02:20 +0100

    skeleton detection live on camera

--- camera_view.py
+++ camera_view.py
@@ -1,5 +1,10 @@
 import cv2
 import sys
+import mediapipe as mp
+
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+pose = mp_pose.Pose()
 
 s = 0
 if len(sys.argv) > 1:
@@ -14,6 +19,10 @@ while cv2.waitKey(1) != 27: # escape
     has_frame, frame = source.read()
     if not has_frame:
         break
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = pose.process(rgb_frame)
+    if result.pose_landmarks:
+        mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     cv2.imshow(win_name, cv2.flip(frame, 1))
 
 source.release()
