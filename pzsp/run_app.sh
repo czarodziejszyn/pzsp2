e35560d22fb4ed86add983b1fd8f578f79479ba1
Julia Czyzewska <01171232@pw.edu.pl>
2025-05-30 14:24:50 +0200

    added scirpt for running the app

---
+++ pzsp/run_app.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd backend
+pip install -r requirements.txt
+
+pip install -r requirements.txt || {
+  echo "pip install failed!"
+  exit 1
+}
+
+python3 run.py &
+BACKEND_PID=$!
+sleep 2
+
+
+cd ../build/web 
+python3 -m http.server 8080 &
+WEB_PID=$!
+
+sleep 2
+
+open http://localhost:8080
+
+
+cleanup() {
+  echo "Stopping servers..."
+  kill $WEB_PID
+  kill $FRONTEND_PID
+  exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+echo "Web server started on http://localhost:8080"
+echo "Press Ctrl+C to stop."
+
+while true; do sleep 1; done
